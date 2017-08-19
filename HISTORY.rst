9603c3072c319e2178e798dd7411a021dda62249
Li Ding <lidingpku@gmail.com>
2017-08-18 23:21:17 -0700

    add autocomplete search box using jquery and jquery-ui

--- HISTORY.rst
+++ HISTORY.rst
@@ -6,3 +6,4 @@ History
 0.2.0 (2017-08-17)
 ++++++++++++++++++
 * add search/autocomplete api using elasticsearch backend (2.4)
+* add autocomplete search box using jquery and jquery-ui 
--- website/docs/cnschema.js
+++ website/docs/cnschema.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // add navigation bar
   $('#leftNav li span').click(function(){
     $('#leftNav li span').removeClass('active');
     $(this).addClass('active');
@@ -20,4 +21,26 @@ $(document).ready(function() {
     $('#aboutContent > ul>li').hide();
     $("#aboutContent >ul >li").eq(index).show();
   })
+
+  // add autocomplete
+  $( "#autocomplete_term" ).autocomplete({
+    source: function(request, response) {
+        $.getJSON("http://cnschema.org:18080/autocomplete",
+            { q: $('#autocomplete_term').val() },
+            function(result){
+                //console.log(JSON.stringify(result));
+                response($.map(result.results, function(item) {
+                  return item
+                }))
+            })
+      },
+      minLength: 1
+    }).data("ui-autocomplete")._renderItem = function (ul, item) {
+      link = '<a href="'+item["@id"]+'">'+item.name+' ('+item.nameZh+')</a>';
+      //console.log(link);
+      return $("<li class='auto-complete-li'></li>")
+        .data("item.autocomplete", item)
+        .append(link)
+        .appendTo(ul);
+    };
 });
